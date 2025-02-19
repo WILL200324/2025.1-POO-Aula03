3ac1b17e58425fe696226d0ff36f08bb39ac07d8
Wilson Souza <wilson.gonsaga@uni9.edu.br>
2025-02-19 13:47:25 +0000

    feat:criar classe televisao

---
+++ aula-03/Program.cs
@@ -0,0 +1,9 @@
+﻿using aula_03;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Televisao tvSala = new Televisao(55);
+
+Console.WriteLine($"O tamanho da tv é: {tvSala.Tamanho}");
+
